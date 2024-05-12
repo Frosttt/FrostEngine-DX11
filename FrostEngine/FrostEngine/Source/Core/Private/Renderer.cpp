37258fda5536564240c512d50d3e67f6bd1a82ef
Chris Dowell <frosttzdev@gmail.com>
2024-05-12 16:39:34 -0700

    Index Buffer added

--- FrostEngine/FrostEngine/Source/Core/Private/Renderer.cpp
+++ FrostEngine/FrostEngine/Source/Core/Private/Renderer.cpp
@@ -106,9 +106,32 @@ void Renderer::DrawTestTriangle()
 		{0.0f, 0.5f , {255, 0, 0, 255}},
 		{0.5f, -0.5f, {0, 255, 0, 255}},
 		{-0.5f, -0.5f, {0, 0, 255, 255}},
-		{0.0f, 0.5f, {255, 0, 0, 255}},
-
+		{-0.3f, 0.3f, {0, 255, 0, 255}},
+		{0.3f, 0.3f, {0, 0, 255, 255}},
+		{0.0f, -0.8f , {255, 0, 0, 255}},
 	};
+
+
+	//// create vertex buffer (2d triangle)
+	//const Vertex vertices[] =
+	//{
+	//	{0.0f, 0.5f , {255, 0, 0, 255}},
+	//	{0.5f, -0.5f, {0, 255, 0, 255}},
+	//	{-0.5f, -0.5f, {0, 0, 255, 255}},
+
+	//	{0.0f, 0.5f , {255, 0, 0, 255}},
+	//	{-0.5f, -0.5f, {0, 0, 255}},
+	//	{-0.3f, 0.3f, {0, 255, 0, 255}},
+
+	//	{0.0f, 0.5f , {255, 0, 0, 255}},
+	//	{0.3f, 0.3f, {0, 0, 255, 255}},
+	//	{0.5f, -0.5f, {0, 255, 0 ,0}},
+
+	//	{0.0f, -0.8f , {255, 0, 0, 255}},
+	//	{-0.5f, -0.5f, {0, 0, 255}},
+	//	{0.5f, -0.5f, {0, 255, 0, 255}},
+	//};
+
 
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	// Build buffer description
@@ -123,13 +146,39 @@ void Renderer::DrawTestTriangle()
 	D3D11_SUBRESOURCE_DATA sd = {};
 	sd.pSysMem = vertices;
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
-	
-	wrl::ComPtr<ID3DBlob> pBlob;
 
 	// bind vertex buffer to pipeline
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	// create index buffer
+
+	const unsigned short indices[] =
+	{
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+
+	};
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT,0u);
+	
+	wrl::ComPtr<ID3DBlob> pBlob;
+
+
 
 	// Create Pixel Shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
@@ -153,7 +202,7 @@ void Renderer::DrawTestTriangle()
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
 			{ "Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 			//{ "Time", 0, DXGI_FORMAT_R32_FLOAT, 0, 16u, D3D11_INPUT_PER_INSTANCE_DATA, 0}
 	};
 
@@ -175,12 +224,12 @@ void Renderer::DrawTestTriangle()
 
 	// configure viewport
 	D3D11_VIEWPORT vp;
-	vp.Width = 800;
-	vp.Height = 600;
+	vp.Width = 400;
+	vp.Height = 300;
 	vp.MinDepth = 0;
 	vp.MaxDepth = 1;
-	vp.TopLeftX = 0;
-	vp.TopLeftY = 0;
+	vp.TopLeftX = 100;
+	vp.TopLeftY = 100;
 	pContext->RSSetViewports(1u, &vp);
 
 	// Set primitive topology
@@ -188,7 +237,8 @@ void Renderer::DrawTestTriangle()
 	pContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST); 
 
 
-	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	//GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u));
 }
 
 void Renderer::EndFrame()
